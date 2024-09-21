13be3c4ecb32c0f75874199da5898126682a3deb
Ayaan <minsu_kim@bishanoi.net>
2024-09-22 00:33:52 +0700

    :zap: feat: Chunk 처리 유연하게

--- src/main.go
+++ src/main.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"net/http"
@@ -16,16 +18,29 @@ import (
 	"golang.org/x/net/proxy"
 )
 
-const (
-	PORT       = 8123
-	CHUNK_SIZE = 10 * 1024 * 1024 // 10MB
-)
+const PORT = 8123
 
 func copyHeader(headerName string, to http.Header, from http.Header) {
 	hdrVal := from.Get(headerName)
 	if hdrVal != "" {
 		to.Set(headerName, hdrVal)
 	}
+}
+
+func copyResponseHeaders(from http.Header, to http.Header) {
+	copyHeader("Accept-Ranges", to, from)
+	copyHeader("Alt-Svc", to, from)
+	copyHeader("Cache-Control", to, from)
+
+	copyHeader("Content-Type", to, from)
+
+	copyHeader("Date", to, from)
+	copyHeader("Expires", to, from)
+	copyHeader("Last-Modified", to, from)
+
+	copyHeader("Server", to, from)
+	copyHeader("Vary", to, from)
+	copyHeader("X-Content-Type-Options", to, from)
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
@@ -37,8 +52,11 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	if method == http.MethodOptions {
 		w.Header().Set("Access-Control-Allow-Origin", headers.Get("Origin"))
 		w.Header().Set("Access-Control-Allow-Methods", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-goog-visitor-id, x-goog-api-key, x-origin, x-youtube-client-version, x-youtube-client-name, x-goog-api-format-version, x-user-agent, Accept-Language, Range, Referer")
+		w.Header().Set("Access-Control-Max-Age", "86400")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 
@@ -53,21 +71,28 @@ func handler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// Set the URL host to the __host parameter
 	urlObj.Scheme = "https"
 	urlObj.Host = urlObj.Query().Get("__host")
 	query := urlObj.Query()
 	query.Del("__host")
 	urlObj.RawQuery = query.Encode()
 
-	// 헤더 복사
+	// Copy headers from the request to the new request
 	requestHeaders := make(http.Header)
 	if headersStr := urlObj.Query().Get("__headers"); headersStr != "" {
 		json.Unmarshal([]byte(headersStr), &requestHeaders)
 	}
-	copyHeader("User-Agent", requestHeaders, headers)
+
+	copyHeader("Range", requestHeaders, headers)
+	if requestHeaders.Get("User-Agent") == "" {
+		copyHeader("User-Agent", requestHeaders, headers)
+	}
+
 	query.Del("__headers")
 	urlObj.RawQuery = query.Encode()
 
+	// Make the request to the target server
 	proxyURL := os.Getenv("PROXY")
 	client := &http.Client{
 		Transport: &http.Transport{
@@ -88,6 +113,8 @@ func handler(w http.ResponseWriter, req *http.Request) {
 		}
 	}
 
+	reqBody, _ := io.ReadAll(req.Body)
+
 	// Construct the return headers
 	responseHeaders := w.Header()
 
@@ -98,63 +125,43 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	responseHeaders.Set("Access-Control-Allow-Credentials", "true")
 	responseHeaders.Set("Cross-Origin-Resource-Policy", "cross-origin")
 
-	// 청크 단위로 요청
-	chunkStart := 0
-	for {
-		chunkEnd := chunkStart + CHUNK_SIZE - 1
-		if chunkEnd < 0 {
-			break
+	// Check the contentLength is smaller than 10MB
+	// If so, set Range header to fetch the whole content
+	if strings.HasSuffix(urlObj.Host, "googlevideo.com") && urlObj.Path == "/videoplayback" {
+		contentLength, _ := strconv.Atoi(urlObj.Query().Get("clen"))
+
+		if contentLength < 10*1024*1024 {
+			requestHeaders.Set("Range", "bytes=0-"+strconv.Itoa(contentLength))
 		}
+	}
 
-		proxyReq, err := http.NewRequest(method, urlObj.String(), nil)
-		if err != nil {
-			log.Fatal("Failed to create request", err)
-			http.Error(w, "Failed to create request", http.StatusInternalServerError)
-			return
-		}
+	// Fetch the response and send it to the client as usual
+	proxyReq, err := http.NewRequest(method, urlObj.String(), strings.NewReader(string(reqBody)))
+	if err != nil {
+		log.Fatal("Failed to create request", err)
+		http.Error(w, "Failed to create request", http.StatusInternalServerError)
+		return
+	}
+	proxyReq.Header = requestHeaders
 
-		// Range 헤더 설정
-		proxyReq.Header = requestHeaders
-		proxyReq.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", chunkStart, chunkEnd))
+	fetchRes, err := client.Do(proxyReq)
+	if err != nil {
+		log.Fatal("Failed to fetch response", err)
+		http.Error(w, "Failed to fetch response", http.StatusInternalServerError)
+		return
+	}
 
-		fetchRes, err := client.Do(proxyReq)
-		if err != nil {
-			log.Fatal("Failed to fetch response", err)
-			http.Error(w, "Failed to fetch response", http.StatusInternalServerError)
-			return
-		}
-		defer fetchRes.Body.Close()
+	defer fetchRes.Body.Close()
 
-		// 응답 처리
-		if fetchRes.StatusCode != http.StatusPartialContent {
-			break
-		}
+	// Send the response headers to the client
+	copyResponseHeaders(fetchRes.Header, responseHeaders)
+	copyHeader("Content-Length", responseHeaders, fetchRes.Header)
+	w.WriteHeader(fetchRes.StatusCode)
 
-		if chunkStart == 0 {
-			copyHeader("Accept-Ranges", responseHeaders, fetchRes.Header)
-			copyHeader("Alt-Svc", responseHeaders, fetchRes.Header)
-			copyHeader("Cache-Control", responseHeaders, fetchRes.Header)
-
-			copyHeader("Content-Length", responseHeaders, fetchRes.Header)
-			// copyHeader("Content-Range", responseHeaders, fetchRes.Header)
-			copyHeader("Content-Type", responseHeaders, fetchRes.Header)
-
-			copyHeader("Date", responseHeaders, fetchRes.Header)
-			copyHeader("Expires", responseHeaders, fetchRes.Header)
-			copyHeader("Last-Modified", responseHeaders, fetchRes.Header)
-
-			copyHeader("Server", responseHeaders, fetchRes.Header)
-			copyHeader("Vary", responseHeaders, fetchRes.Header)
-			copyHeader("X-Content-Type-Options", responseHeaders, fetchRes.Header)
-		}
-
-		// 응답 스트림을 클라이언트에 전달
-		if _, err := io.Copy(w, fetchRes.Body); err != nil {
-			log.Printf("Error writing response: %v", err)
-			return
-		}
-
-		chunkStart += CHUNK_SIZE // 다음 청크로 이동
+	// Send the response body
+	if _, err := io.Copy(w, fetchRes.Body); err != nil {
+		log.Printf("Error writing response: %v", err)
+		return
 	}
 }
 
